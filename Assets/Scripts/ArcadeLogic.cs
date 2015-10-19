61f90aba8c6344b7b338a97daab5f22eff476cb9
dMorenov <danielmorenotvjb42013@cev.com>
2015-10-19 13:06:03 +0200

    arcadegameplay++

--- Assets/Scripts/ArcadeLogic.cs
+++ Assets/Scripts/ArcadeLogic.cs
@@ -4,19 +4,23 @@ using UnityEngine.UI;
 
 public class ArcadeLogic : MonoBehaviour {
 
-    public Text timeTxt, limitTxt;
+    public Text timeTxt, limitTxt, maxTime;
     public Slider limitSlider;
+    public GameObject defeatCanvas, startCanvas;
 
     private float currentTime;
     private float limitTime;
-    private float currentLimit;
+    private float levelTimer;
+    public bool playing;
+    public bool defeat;
 
 	// Use this for initialization
 	void Start () 
     {
+        playing = false;
         currentTime = 0;
         limitTime = 1.5f;
-        currentLimit = limitTime;
+        levelTimer = 0;
         limitSlider.maxValue = limitTime;
         limitSlider.value = limitSlider.maxValue;
 	}
@@ -24,14 +28,54 @@ public class ArcadeLogic : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
     {
-        currentTime += Time.deltaTime;
-        limitSlider.value -= Time.deltaTime;
-        if (limitSlider.value <= 0)
+        if (playing)
         {
-            limitSlider.value = limitSlider.maxValue;
+            currentTime += Time.deltaTime;
+            limitSlider.value -= Time.deltaTime;
+            levelTimer += Time.deltaTime;
+            if (limitSlider.value <= 0)
+            {
+                playing = false;
+                Defeat();
+            }
+
+            if (levelTimer >= 6) UpdateLevel();
+    
+
+            timeTxt.text = "Time: " + currentTime.ToString("00.00");
+            limitTxt.text = "Limit: " + limitTime.ToString("0.000") + "s";
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                //limitTime *= 0.95f;
+                //currentLimit = limitTime;
+                //limitSlider.maxValue = limitTime;
+                limitSlider.value = limitSlider.maxValue;
+            }
+
+
         }
 
-        timeTxt.text = "Time: " + currentTime.ToString("00.00");
-        limitTxt.text = "Limit: " + currentLimit.ToString("0.000") + "s";
+        if (Input.GetButton("Jump")) playing = true;
+        
 	}
+
+    void UpdateLevel()
+    {
+        limitTime *= 0.95f;
+        limitSlider.maxValue = limitTime;
+        levelTimer = 0;
+    }
+
+    void Defeat()
+    {
+        if (defeatCanvas.activeSelf == false) defeatCanvas.SetActive(true);
+        maxTime.text = "You lasted " + currentTime.ToString("00.00") + " seconds!";
+    }
+
+    public void Reload()
+    {
+        Application.LoadLevel("ArcadeLevel");
+    }
+
 }
