e1ec89adb1a598096bbc016d9db7dd5177dc8337
Marc <magueracoras@gmail.com>
2015-10-26 18:26:14 +0100

    Daikon forge and camera shaders

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+    Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+        myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myTransform.Rotate(new Vector3(0, 0, 6 * Time.deltaTime));
+	}
+}
