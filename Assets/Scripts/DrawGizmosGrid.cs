bb0009a3b3692333fb5edc76d154a87e75af66bb
Marc <magueracoras@gmail.com>
2015-10-29 11:30:13 +0100

    array grid

---
+++ Assets/Scripts/DrawGizmosGrid.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class DrawGizmosGrid : MonoBehaviour {
+
+	public Vector2 gridSize;
+	public float cellSize;
+	
+
+    void OnDrawGizmos()
+    {
+		Gizmos.DrawWireCube(transform.position, new Vector3(gridSize.x, gridSize.y, 1));
+
+		Vector3 worldBottomLeft = transform.position - Vector3.right * gridSize.x / 2 - Vector3.up * gridSize.y / 2;
+
+		for (int x = 0; x < gridSize.x; x++)
+        {
+			for (int y = 0; y < gridSize.y; y++)
+            {
+				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * cellSize*2 + cellSize) + Vector3.up * (y * cellSize * 2 + cellSize);
+				Gizmos.DrawWireCube(worldPoint, new Vector3(cellSize * 2, cellSize * 2, 1));
+            }
+        }
+    }
+}
--- Assets/Scripts/ObjectsArray.cs
+++ Assets/Scripts/ObjectsArray.cs
@@ -7,19 +7,23 @@ public class ObjectsArray : MonoBehaviour {
     public ParticleSystem destroyParticles;
     public ParticleSystem[] destroyParticlesArray;
     //public GameObject noTarget;
-    private GameObject[] m_targetArray;
+    private GameObject[,] m_targetArray;
     public bool playing;
     public float currentTime, maxTime, maxRan, minRan;
     public int maxTarget;
     public Transform top, bot, left, rigth;
     private Vector3 m_position;
+    private Vector3[] m_positionArray;
     public int lifes = 3;
+    public Vector2 wordSize = new Vector2 (8,4);
     public GameObject[] lifesImage = new GameObject[3];
 
 	// Use this for initialization
 	void Start () {
-        m_targetArray = new GameObject[maxTarget];
+        maxTarget = (int)(wordSize.x * wordSize.y);
+        m_targetArray = new GameObject[(int)wordSize.x, (int)wordSize.y];
         destroyParticlesArray = new ParticleSystem[maxTarget];
+        m_positionArray = new Vector3[maxTarget];
         CreateArray(m_targetArray, target, destroyParticlesArray, destroyParticles);
         //ActivateTarget();
         minRan = 0.5f;
@@ -47,40 +51,58 @@ public class ObjectsArray : MonoBehaviour {
         }
 	}
 
-    void CreateArray(GameObject[] tArray, GameObject tGameObject, ParticleSystem[] pArray, ParticleSystem pSystem)
+    void CreateArray(GameObject[,] tArray, GameObject tGameObject, ParticleSystem[] pArray, ParticleSystem pSystem)
     {
-        for (int i = 0; i < maxTarget; i++)
+        Vector3 worldBottomLeft = transform.position - Vector3.right * wordSize.x / 2 - Vector3.up * wordSize.y / 2;
+
+        for (int x = 0; x < wordSize.x; x++)
         {
-            GameObject t;
-            ParticleSystem p;
-            t = (GameObject)Instantiate(tGameObject, Vector3.one * 20, Quaternion.identity);
-            p = (ParticleSystem)Instantiate(pSystem, Vector3.one * 20, Quaternion.identity);
-            //targetAnimation[i] = t.GetComponent<TargetAnimation>();
-            //m_material[i] = t.GetComponent<SpriteRenderer>();
-            t.SetActive(false);
-            tArray[i] = t;
-            pArray[i] = p;
+            for (int y = 0; y < wordSize.y; y++)
+            {
+                GameObject t;
+                ParticleSystem p;
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * 2 + 1f) + Vector3.up * (y * 2 + 1f);
+                t = (GameObject)Instantiate(tGameObject, worldPoint, Quaternion.identity);
+                p = (ParticleSystem)Instantiate(pSystem, Vector3.one, Quaternion.identity);
+                
+                //t.transform.position = worldPoint;
+
+                //targetAnimation[i] = t.GetComponent<TargetAnimation>();
+                //m_material[i] = t.GetComponent<SpriteRenderer>();
+                t.SetActive(false);
+
+                tArray[x, y] = t;
+                pArray[x] = p;
+                
+            }
         }
+
     }
 
     public void ActivateTarget()
     {
         int counter = 0;
         //currentTime += Time.deltaTime;
-        
-        
-        for (int i = 0; i < maxTarget; i++)
+        int x = Mathf.FloorToInt( Random.Range(0f, wordSize.x));
+        int y = Mathf.FloorToInt(Random.Range(0f, wordSize.y));
+
+        if (counter < 1 && !m_targetArray[x, y].activeInHierarchy)
         {
-            if (counter < 1 && !m_targetArray[i].activeInHierarchy)
-            {
-                m_position = new Vector3(Random.Range((int)left.position.x, (int)rigth.position.x), Random.Range((int)bot.position.y, (int)top.position.y), 0);
-                    m_targetArray[i].transform.position = m_position;
-                    m_targetArray[i].SetActive(true);
-                         
-                //tArray[i].GetComponent<SpriteRenderer>().color = targetColor[Random.Range(0, targetColor.Length)];
-                counter++;
-            }
+            //m_position = new Vector3(Random.Range((int)left.position.x, (int)rigth.position.x), Random.Range((int)bot.position.y, (int)top.position.y), 0);
+            //m_positionArray[i] = m_position;
+
+            //m_targetArray[i].transform.position = m_position;
+            m_targetArray[x, y].SetActive(true);
+
+            //tArray[i].GetComponent<SpriteRenderer>().color = targetColor[Random.Range(0, targetColor.Length)];
+            counter++;
         }
+        else if(m_targetArray[x, y].activeInHierarchy)
+        {
+            x = Mathf.FloorToInt(Random.Range(0f, wordSize.x));
+            y = Mathf.FloorToInt(Random.Range(0f, wordSize.y));
+        }
+        
         //currentTime = 0;
         //realocating = false;
    
