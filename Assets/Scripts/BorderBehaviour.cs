a283e62ca32eb8f3ef6adcfc43ef0237e14706ac
dMorenov <danielmorenotvjb42013@cev.com>
2015-11-05 10:58:55 +0100

    Menu borders

---
+++ Assets/Scripts/BorderBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BorderBehaviour : MonoBehaviour {
+
+    private Transform _myTrans;
+    private Vector3 _scaleRate, _initScale;
+    public Color color;
+    SpriteRenderer _sprite;
+
+	// Use this for initialization
+	void Start () 
+    {
+        _myTrans = GetComponent<Transform>();
+        _scaleRate = new Vector3(4, 4, 0);
+        //_myTrans.localScale = new Vector3(32, 18, 1);
+        _sprite = GetComponent<SpriteRenderer>();
+        _sprite.color = color;
+        _initScale = new Vector3(26, 26, 1);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        _myTrans.localScale -= _scaleRate * Time.deltaTime;
+        if (_myTrans.localScale.x <= 17)
+        {
+            color.a -= 0.8f * Time.deltaTime;
+            _sprite.color = color;
+
+            if (color.a <= 0)
+            {
+                Reset();
+            }
+        }
+
+        
+	}
+
+    void Reset()
+    {
+        _myTrans.localScale = _initScale;
+        color.a = 1;
+        _sprite.color = color;
+    }
+}
